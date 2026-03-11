b26c01decb318a59a4971871049d4fdb3e5ed17c
ceejaybabadilla <ceejaybabadilla2@gmail.com>
2026-03-11 14:20:07 +0800

    update 2

--- REAL ESTATE/REAL ESTATE/script.js
+++
@@ -1,7 +0,0 @@
-const toggleMenu = document.querySelector(".toggle_menu");
-const headerNav = document.querySelector(".header__nav");
-
-toggleMenu.addEventListener("click", () => {
-  toggleMenu.classList.toggle("open");
-  headerNav.classList.toggle("open");
-});
